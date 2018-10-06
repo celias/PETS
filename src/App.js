1690360324b534e514a66c016e9ee612456f4a38
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-10-05 21:06:24 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import pf from 'petfinder-client';
 import Pet from './Pet';
 
-class App extends React.Component {
-	handleTitleClick() {
-		alert('You clicked the title!');
-	}
+const petfinder = pf({
+	key: process.env.API_KEY,
+	secret: process.env.API_SECRET
+});
 
+class App extends React.Component {
+	componentDidMount() {}
 	render() {
-		// return React.createElement('div', {}, [
-		//     React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me!'),
-		//     React.createElement(Pet, {
-		// name: 'Lu',
-		// animal: 'cat',
-		// breed: 'Black Tabby'
-		//     }),
-		//     React.createElement(Pet, {
-		//         name: 'Remy',
-		//         animal: 'dog',
-		//         breed: 'Miniature Australian Shepherd'
-		//     }),
-		//     React.createElement(Pet, {
-		//         name: 'Sabrina',
-		//         animal: 'cat',
-		//         breed: 'Tortoiseshell'
-		//     })
-		// ]);
 		return (
 			<div>
 				<h1>Adopt me!</h1>
