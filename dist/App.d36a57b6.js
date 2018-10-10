d024cd096b33f676362d3a72af1e41a1561e8fa0
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-10-10 16:23:18 -0500

    changes

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -33049,7 +33049,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49708" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54436" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
